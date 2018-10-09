7ab67dd2a67334947b5d9fbc2894f3041f926dc3
zhuliyi <2280835569@qq.com>
2018-10-10 00:47:38 +0800

    添加构造神经网络

--- tensorflow_demo/lession5.py
+++ tensorflow_demo/lession5.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -10,3 +11,23 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
     else:
         outputs = activation_function(Wx_plus_b)
     return outputs
+
+
+x_data = np.linspace(-1, 1, 300,dtype=np.float32)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
+y_data = np.square(x_data) - 0.5 + noise
+
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+predition = add_layer(l1, 10, 1, activation_function=None)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - predition), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
